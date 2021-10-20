23b84f4622f50a293319c8f0b670a5b6102e17e5
Roman Šimík <roman@krm22.com>
2021-10-21 01:13:19 +0200

    Add support for changing letsEncrypt environment

--- variables.tf
+++ variables.tf
@@ -40,6 +40,12 @@ variable "name" {
   description = "Name for deployment"
 }
 
+variable "letsencrypt_environment" {
+  type        = string
+  default     = "production"
+  description = "LetsEncrypt environment to use ('production' or 'staging')"
+}
+
 variable "letsencrypt_email" {
   type        = string
   default     = "none@none.com"
